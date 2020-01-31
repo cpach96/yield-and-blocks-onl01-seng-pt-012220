0d900c44b37f297ea46595f9fa9732b99ce1ed73
Curtis pachtinger <cpach96@gmail.com>
2020-01-31 01:38:57 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,6 @@
 def hello_t(array)
-  
+  if block_given?
+    
   i = 0 
    while i  < array.length 
       yield array[i]
@@ -7,4 +8,7 @@ def hello_t(array)
      
    end
    array
+   
+  else
+    puts "Hey! No block was given!"
 end
